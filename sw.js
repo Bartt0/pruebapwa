ed5d2968202fa0d313859d7c2e1605471669b806
Bartt0 <fide_arias_zavala@hotmail.com>
2022-11-17 08:21:32 -0600

    Prueba de una pwa creacion de manifest y Service worker

---
+++ sw.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'my-app-cache-v1';
+
+self.addEventListener('install', event => {
+  event.waitUntil(( async () => {
+    const cache = await caches.open(CACHE_NAME);
+    cache.addAll(['/']);
+    })());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(( async () => {
+        const cache = await caches.open(CACHE_NAME);
+
+        try{
+            const fetchResponse = await fetch(event.request);
+
+            cache.put(event.request, fetchResponse.clone());
+        
+            return fetchResponse;
+
+        }catch (e){
+            const cachedResponse = await cache.match(event.request);
+            return cachedResponse;
+        }
+    })());
+});
